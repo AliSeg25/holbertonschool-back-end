2a4a541520c7b14389c582992de6ecd8df9fb0d6
Your Name <you@example.com>
2023-04-05 11:04:41 +0200

    task2

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Write a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import requests
+import sys
+import json
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        employee_id = int(sys.argv[1])
+    else:
+        print("Usage: ./2-export_to_JSON.py <employee_id>")
+        sys.exit(1)
+
+    # On va chercher l'utilisateur et sa todo
+    response_url = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    response_dict = response_url.json()
+    employee_name = response_dict.get("name")
+
+    response_url = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+    response_dict = response_url.json()
+
+    print(response_dict)
+    tasks = []
+    for task in response_dict:
+        task_dict = {}
+        task_dict["task"] = task["title"]
+        task_dict["completed"] = task["completed"]
+        task_dict["username"] = employee_name
+        tasks.append(task_dict)
+
+    # Exportation des résultats au format JSON
+    filename = f"{employee_id}.json"
+    with open(filename, mode='w') as f:
+        json.dump({employee_id: tasks}, f)
