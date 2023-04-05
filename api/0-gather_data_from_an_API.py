2d76d5ee11f9a0b3fe85112930efd22a4c025070
Your Name <you@example.com>
2023-04-05 09:16:57 +0200

    taskO

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Write a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import requests
+import sys
+
+
+def Return_employer_todolist(employee_id):
+    # On va chercher l'utilisateur et sa todo
+    #response_url = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+    #response_dict = response_url.json()
+    response_dict = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos").json()
+    #print(response_dict)
+
+    # On va compter le nombre de task fait et fini
+    count, total = 0, 0
+    completed_tasks = []
+    for task in response_dict:
+        total += 1
+        if task['completed'] is True:
+            count += 1
+            completed_tasks.append(task['title'])
+
+    employee_name = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}").json().get("name")
+    print(f"Employee {employee_name} is done with tasks({count}/{total}):")
+    for task in completed_tasks:
+        print("\t " + task)
+    
+
+Return_employer_todolist(1)
