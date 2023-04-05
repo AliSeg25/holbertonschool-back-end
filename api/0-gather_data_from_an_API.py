eb5b98fdd3daf3a44b0468884841ed0069e5fff1
Your Name <you@example.com>
2023-04-05 09:47:26 +0200

    task0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,14 +8,23 @@ import sys
 
 
 if __name__ == "__main__":
-    # Récupérer l'ID de l'employé à partir de la ligne de commande
-    employee_id = sys.argv[1]
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        employee_id = int(sys.argv[1])
+    else:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
 
     # On va chercher l'utilisateur et sa todo
-    response_dict = requests.get(f"https://jsonplaceholder.typicode.com/users/\
-    {employee_id}/todos").json()
+    response_url = requests.get(f"https://jsonplaceholder.typicode.com/users/\
+    {employee_id}")
+    response_dict = response_url.json()
+    employee_name = response_dict.get("name")
 
-    # On va compter le nombre de task fait et fini
+    response_url = requests.get(f"https://jsonplaceholder.typicode.com/users/\
+    {employee_id}/todos")
+    response_dict = response_url.json()
+
+    # On va compter le nombre de tâches faites et finies
     count, total = 0, 0
     completed_tasks = []
     for task in response_dict:
@@ -24,8 +33,7 @@ if __name__ == "__main__":
             count += 1
             completed_tasks.append(task['title'])
 
-    employee_name = requests.get(f"https://jsonplaceholder.typicode.com/users/\
-    {employee_id}").json().get("name")
+    # Affichage des résultats
     print(f"Employee {employee_name} is done with tasks({count}/{total}):")
     for task in completed_tasks:
         print("\t " + task)
