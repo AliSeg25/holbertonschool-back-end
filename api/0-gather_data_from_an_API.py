61d6019595433ed10e66ee8119a86d87dbe600db
Your Name <you@example.com>
2023-04-05 09:49:48 +0200

    task3

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
         print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
-    # On va chercher l'utilisateur et sa todo
-    response_url = requests.get(f"https://jsonplaceholder.typicode.com/users/\
-    {employee_id}")
+        # On va chercher l'utilisateur et sa todo
+    response_url = requests.get(f"https://jsonplaceholder.typicode.com/\
+    users/{employee_id}")
     response_dict = response_url.json()
     employee_name = response_dict.get("name")
 
-    response_url = requests.get(f"https://jsonplaceholder.typicode.com/users/\
-    {employee_id}/todos")
+    response_url = requests.get(f"https://jsonplaceholder.typicode.com/\
+    users/{employee_id}/todos")
     response_dict = response_url.json()
 
     # On va compter le nombre de tâches faites et finies
